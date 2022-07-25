95923abb58ae82273f0242a0f7a4714584528bf2
MadCoderr <msrfarooqi@gmail.com>
2022-07-26 00:20:39 +0500

    3 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 import { faker } from '@faker-js/faker';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 interface IListItem {
   id: number;
@@ -11,6 +12,8 @@ interface IListItem {
 function App() {
   const [items, setItems] = useState<IListItem[]>([]);
 
+  const [listRef] = useAutoAnimate<HTMLUListElement>();
+
   const addNewItem = () => {
     const newItem: IListItem = {
       title: faker.lorem.sentence(3),
@@ -19,15 +22,20 @@ function App() {
     setItems((_items) => [..._items, newItem]);
   };
 
+  const removeItem = (_id: number) => {
+    const filterList = items.filter((item) => item.id !== _id);
+    setItems(filterList);
+  };
+
   return (
     <div className="root">
       <button className="add-btn" onClick={addNewItem}>
         Add item
       </button>
 
-      <ul className="list">
+      <ul className="list" ref={listRef}>
         {items.map((item: IListItem) => (
-          <li key={item.id}>
+          <li key={item.id} onClick={() => removeItem(item.id)}>
             <span className="label">{item.title}</span>
             <span className="delete-btn">X</span>
           </li>
